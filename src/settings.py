8630bee01cf67a8f6db79dab353529c884dee48f
awitherow <au.witherow@gmail.com>
2017-08-05 21:47:54 +0200

    add api keys for bittrex

--- src/settings.py
+++ src/settings.py
@@ -5,4 +5,9 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
 
+# telegram bot
 telegram_token = environ.get("bot_api_token")
+
+# bittrex api
+rex_api_key = environ.get("bittrex_api_key")
+rex_api_secret = environ.get("bittrex_api_secret")
