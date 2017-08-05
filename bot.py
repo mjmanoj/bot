9121c3835baad6a44f7afdd175d0b44eb379953c
awitherow <au.witherow@gmail.com>
2017-08-05 20:55:56 +0200

    remove temp unneeded stuff

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 import telegram
-from telegram.ext import Updater
 
 from settings import telegram_token
 
 bot = telegram.Bot(token=telegram_token)
-updater = Updater(token=telegram_token)
 
 print(bot.get_me())
