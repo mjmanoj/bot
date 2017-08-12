6b2f4b5ecff23817b3cb58bbaada4961cdcc2a73
awitherow <au.witherow@gmail.com>
2017-08-12 20:21:36 +0200

    be a little pickier on which coins are checked

--- src/rex.py
+++ src/rex.py
@@ -10,18 +10,39 @@ Rex = bittrex.Bittrex(api_key="", api_secret="")
 blacklist = ["GLD", "1ST"]
 
 
+def cream_only(list):
+    return int(len(list) * 0.4)
+
+
 def get_market_summaries():
     res = Rex.get_market_summaries()
-    summaries = []
+
+    btc_summaries = []
+    eth_summaries = []
+    usdt_summaries = []
 
     for summary in reversed(sorted(res["result"], key=itemgetter("Volume"))):
-        coin = summary["MarketName"].split("-")[0]
-        if coin == "BTC" or coin == "USDT" or coin == "ETH":
-            summaries.append(summary["MarketName"].split("-")[1])
+        market = summary["MarketName"].split("-")[0]
+        coin = summary["MarketName"].split("-")[1]
+        volume = summary["Volume"]
+
+        # low volume trimming
+        if market == "BTC":
+            btc_summaries.append(coin)
+
+        if market == "ETH":
+            eth_summaries.append(coin)
+
+        if market == "USDT":
+            usdt_summaries.append(coin)
+
+    summaries = btc_summaries[:cream_only(btc_summaries)] + eth_summaries[:cream_only(
+        eth_summaries)] + usdt_summaries[:cream_only(usdt_summaries)]
 
     # get rid of blacklist terms (I have found these overlap and pollute data, sorry!)
     for blacklisted in blacklist:
-        summaries.remove(blacklisted)
+        if blacklisted in summaries:
+            summaries.remove(blacklisted)
 
     # ensure no duplicates
     final = []
