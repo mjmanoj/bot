bae7a633e90bf08a49a95ba4b71a8ea050739d33
awitherow <au.witherow@gmail.com>
2017-08-13 10:11:55 +0200

    comment out non working woeids

--- src/constants.py
+++ src/constants.py
@@ -6,7 +6,7 @@ DEFAULT_MINS = 30
 HOT_COUNTRIES = [
     23424977,  # USA
     23424936,  # Russia
-    23424781,  # China
+    # 12578011,  # Bejing
     23424856,  # Japan
     23424848,  # India
     23424829,  # Germany
@@ -15,8 +15,8 @@ HOT_COUNTRIES = [
     23424775,  # Canada
     23424948,  # Singapore, maybe 1062617?? two entries
     23424954,  # Sweden
-    23424812,  # Finland
-    24554868,  # England
+    # 23424812,  # Finland
+    44418,  # London
     23424819,  # France
     23424934,  # Phillipines
 ]
