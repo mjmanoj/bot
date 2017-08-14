ffdf6394f725955ecdc205087e499a7706a86217
awitherow <au.witherow@gmail.com>
2017-08-14 22:45:12 +0200

    upload db on every run

--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-while true; do make execute; sleep 1800; done
---
+++ scripts/upload_db.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git add db/prod/*
+git commit -m "chore: upload production database"
+git push origin master
