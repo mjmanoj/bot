34e83a49d03d9db74fb5213c50b40276a5857521
awitherow <au.witherow@gmail.com>
2017-08-23 20:03:53 +0200

    ensure no empty arrays

--- src/archivist.py
+++ src/archivist.py
@@ -63,25 +63,26 @@ def get_twitter_res_time(time_range):
 
     res_time = 0
 
-    if time_range == "last":
-        last_op = sorted_ops[0]
+    if sorted_ops is None:
+        if time_range == "last":
+            last_op = sorted_ops[0]
 
-        if last_op:
-            start = int(last_op["twitter_search_start"])
-            end = int(last_op["twitter_search_end"])
-            last_twitter_call_seconds = abs(start - end)
-            res_time += last_twitter_call_seconds
-            print "[INFO] last twitter response time was " + str(last_twitter_call_seconds) + " seconds."
+            if last_op:
+                start = int(last_op["twitter_search_start"])
+                end = int(last_op["twitter_search_end"])
+                last_twitter_call_seconds = abs(start - end)
+                res_time += last_twitter_call_seconds
+                print "[INFO] last twitter response time was " + str(last_twitter_call_seconds) + " seconds."
 
-    if time_range == "average" and sorted_ops is not None:
-        durations = 0
+        if time_range == "average" and sorted_ops is not None:
+            durations = 0
 
-        for operation in sorted_ops:
-            start = int(operation["twitter_search_start"])
-            end = int(operation["twitter_search_end"])
-            durations += abs(start - end)
+            for operation in sorted_ops:
+                start = int(operation["twitter_search_start"])
+                end = int(operation["twitter_search_end"])
+                durations += abs(start - end)
 
-        res_time += durations / len(sorted_ops)
-        print "[INFO] average twitter response time " + res_time + " seconds."
+            res_time += durations / len(sorted_ops)
+            print "[INFO] average twitter response time " + res_time + " seconds."
 
     return res_time
