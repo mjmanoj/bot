a0d2bd42942b83f068f8c329df0ed62318c857f8
awitherow <au.witherow@gmail.com>
2017-08-28 11:38:58 +0200

    feature: get update changes to post to channel

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 while true; do
 
+    LAST=git rev-list master --max-count=1
+    git reset --hard origin/master
+    RECENT=git rev-list master --max-count=1
+    if ["$LAST -ne $RECENT"]
+    then
+        source .env
+        MESSAGE="git log $RECENT..$LAST"
+        prod_channels=($telegram_chat_prod $kirby_bot_channel)
+        dev_channels=($telegram_chat_dev)
+        # get list of channels for appropriate environment
+        # for each, curl that shit.
+        curl -i -X GET "https://api.telegram.org/bot$telegram_token/sendMessage?chat_id=$&text=$MESSAGE"
+    fi
+
     make execute
     make upload_db_to_github
     
