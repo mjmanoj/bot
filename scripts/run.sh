c00583ec82157cf69f6ed757916a5a9bddbf5e98
awitherow <au.witherow@gmail.com>
2017-08-28 13:37:07 +0200

    feature: curl send message to each of the channels defined

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,15 +4,25 @@ while true; do
     LAST=git rev-list master --max-count=1
     git reset --hard origin/master
     RECENT=git rev-list master --max-count=1
+    
     if ["$LAST -ne $RECENT"]
     then
         source .env
         MESSAGE="git log $RECENT..$LAST"
-        prod_channels=($telegram_chat_prod $kirby_bot_channel)
-        dev_channels=($telegram_chat_dev)
-        # get list of channels for appropriate environment
-        # for each, curl that shit.
-        curl -i -X GET "https://api.telegram.org/bot$telegram_token/sendMessage?chat_id=$&text=$MESSAGE"
+        PROD_CHANNELS=($telegram_chat_prod $kirby_bot_channel)
+        DEV_CHANNELS=($telegram_chat_dev)
+        CHANNELS=()
+        case "$MOONBOT_ENV" in
+            "test")
+                CHANNELS=$DEV_CHANNELS
+            "prod")
+                CHANNELS=$PROD_CHANNELS
+        esac
+
+        for CHANNEL in CHANNELS
+        do
+            curl -i -X GET "https://api.telegram.org/bot$telegram_token/sendMessage?chat_id=$CHANNEL&text=$MESSAGE"
+        done
     fi
 
     make execute
