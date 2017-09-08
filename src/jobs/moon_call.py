ee8559be3e5dc62e10431ebe1ebffdb9e9111012
awitherow <au.witherow@gmail.com>
2017-09-08 15:55:28 +0200

    fix imports

--- src/jobs/moon_call.py
+++ src/jobs/moon_call.py
@@ -4,23 +4,20 @@ the main package runs the main functionalities of the program
 - moon_call is a function to call moon shots on market symbols
 """
 from operator import itemgetter
-
-import postgres
-from config import env
-from archivist import get_moon_call_res_duration, get_score_history
-from helpers import get_time_now
-from bot import generate_and_post_message
-from twit import search, get_trends_for_woeid
-from constants import HOT_COUNTRIES
-import rex
-import logician
+from src import archivist
+from src import helpers
+from src import bot
+from src import twit
+from src import rex
+from src import logician
+from src import postgres
 
 
 def moon_call():
     """ call hot shots on market symbols """
 
     operations_log = {}
-    operations_log["init"] = get_time_now(stringify=True)
+    operations_log["init"] = helpers.get_time_now(stringify=True)
 
     print("[JOB] Starting moon_call at " + operations_log["init"])
 
@@ -28,20 +25,21 @@ def moon_call():
     scores = []
 
     print("[JOB] Searching Twitter for BTRX symbol high volume list...")
-    operations_log["twitter_search_start"] = get_time_now(stringify=True)
+    operations_log["twitter_search_start"] = helpers.get_time_now(
+        stringify=True)
 
-    avg_res = get_moon_call_res_duration()
+    avg_res = archivist.get_moon_call_res_duration()
 
     print("[JOB] Scoring " + str(len(summaries)) + " coins...")
     # get and score relevant tweets per symbol.
     for summary in summaries:
         entry = summary
-        entry["created"] = get_time_now(stringify=True)
+        entry["created"] = helpers.get_time_now(stringify=True)
 
         coin_symbol = "$" + summary["symbol"]
 
         # search twitter
-        tweets = search(coin_symbol)
+        tweets = twit.search(coin_symbol)
         score = logician.judge(tweets, stale_break=avg_res + 3200)
 
         # if score sucks, go to next symbol
@@ -52,7 +50,7 @@ def moon_call():
         postgres.add_coin_symbol(entry)
         scores.append(entry)
 
-    operations_log["twitter_search_end"] = get_time_now(stringify=True)
+    operations_log["twitter_search_end"] = helpers.get_time_now(stringify=True)
 
     # sort and find hottest trends
     sorted_scores = sorted(scores, key=itemgetter("score"), reverse=True)
@@ -60,22 +58,22 @@ def moon_call():
 
     print("[JOB] Preparing message templates...")
 
-    daily_top_scores = get_score_history(tf="day")
-    weekly_top_scores = get_score_history(tf="week")
+    daily_top_scores = archivist.get_score_history(tf="day")
+    weekly_top_scores = archivist.get_score_history(tf="week")
 
     # prepare message for telegram
-    operations_log["send_message_end"] = get_time_now(stringify=True)
+    operations_log["send_message_end"] = helpers.get_time_now(stringify=True)
 
-    generate_and_post_message(
+    bot.generate_and_post_message(
         hourly_top_scores, daily_top_scores, weekly_top_scores)
 
-    operations_log["send_message_end"] = get_time_now(stringify=True)
+    operations_log["send_message_end"] = helpers.get_time_now(stringify=True)
 
     print("[JOB] Moon call complete, message sent at " +
-          get_time_now(stringify=True))
+          helpers.get_time_now(stringify=True))
     print("[JOB] Sleeping now for one hour...\n\n")
 
-    operations_log["end"] = get_time_now(stringify=True)
+    operations_log["end"] = helpers.get_time_now(stringify=True)
     postgres.add_operations_log(operations_log)
 
 
