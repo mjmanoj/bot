0ec9d959c28460341bfd10c2a8179da48ed7803d
awitherow <au.witherow@gmail.com>
2017-09-08 22:13:29 +0200

    use contextual twitter client

--- src/twit.py
+++ src/twit.py
@@ -5,18 +5,34 @@ the twit package is a bear python-twitter adapter
 import twitter
 from config import twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_secret
 
-API = twitter.Api(consumer_key=twitter_consumer_key,
-                  consumer_secret=twitter_consumer_secret,
-                  access_token_key=twitter_access_token,
-                  access_token_secret=twitter_access_secret)
+
+class API():
+    """ contextual twitter api for setting up and tearing down before running functional methods."""
+
+    # intilize
+    def __init__(self):
+        self.client = twitter.Api(consumer_key=twitter_consumer_key,
+                                  consumer_secret=twitter_consumer_secret,
+                                  access_token_key=twitter_access_token,
+                                  access_token_secret=twitter_access_secret)
+
+    # setup
+    def __enter__(self):
+        return self.client
+
+    # teardown
+    def __exit__(self, *args):
+        self.client = None
 
 
 def search(term):
     """ get_recent_tweets_with_search_term returns 100 search results per term """
 
-    return API.GetSearch(term=term, count=100, result_type="recent")
+    with API() as api:
+        return api.GetSearch(term=term, count=100, result_type="recent")
 
 
 def get_tweep(tweep):
     """ gets a twitter user, aka tweep. """
-    return API.GetUser(tweep)
+    with API() as api:
+        return api.GetUser(tweep)
